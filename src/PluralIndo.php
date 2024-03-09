3b6d4ad8b40ece7ff8a3c15336ee53d3534bd9d3
Zakki Mahatir <zakkimahatir77@gmail.com>
2024-03-09 13:03:18 +0700

    first commit

---
+++ src/PluralIndo.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Zakki\PluralIndo;
+
+use Doctrine\Inflector\InflectorFactory;
+use Doctrine\Inflector\Rules\Pattern;
+use Doctrine\Inflector\Rules\Patterns;
+use Doctrine\Inflector\Rules\Ruleset;
+use Doctrine\Inflector\Rules\Substitution;
+use Doctrine\Inflector\Rules\Substitutions;
+use Doctrine\Inflector\Rules\Transformation;
+use Doctrine\Inflector\Rules\Transformations;
+use Doctrine\Inflector\Rules\Word;
+
+include 'vendor/autoload.php';
+
+class PluralIndo {
+
+    public function Plural()
+    {
+    
+        $inflector = InflectorFactory::create()
+            ->withPluralRules(
+                new Ruleset(
+                    new Transformations(
+                        new Transformation(new Pattern('^(\w+)$'), '\1 - \1'),
+                    ),
+                    new Patterns(new Pattern('noflect'), new Pattern('abtuse')),
+                    new Substitutions(
+                        new Substitution(new Word('rumah'), new Word('perumahan')),
+                        new Substitution(new Word('phone'), new Word('phonezes'))
+                    )
+                )
+            )
+            ->build();
+    
+            return $inflector;
+    }
+}
+
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'zakki/plural',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => null,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'doctrine/inflector' => array(
+            'pretty_version' => '2.0.10',
+            'version' => '2.0.10.0',
+            'reference' => '5817d0659c5b50c9b950feb9af7b9668e2c436bc',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../doctrine/inflector',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'zakki/plural' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => null,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
